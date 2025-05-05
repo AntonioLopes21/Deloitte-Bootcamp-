3a14d08294eb480914df78fa1b561435daef9aa4
bootcamp02 <bootcamp02@deloitte.com>
2025-05-05 17:53:06 -0300

    desafio de casa

---
+++ src/dia01/OO/Pessoa.java
@@ -0,0 +1,14 @@
+package dia01.OO;
+
+public class Pessoa {
+    private String nome;
+    private int idade;
+}
+
+public void compararIdade(Pessoa pessoa1, Pessoa pessoa2) {
+
+}
+
+public void verificarMaioridade() {
+
+}
